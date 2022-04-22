adb5874fd0c66bd5044720ca68b83e10297e43a1
Estuful <100486956+Estuful@users.noreply.github.com>
2022-04-22 17:24:04 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+ <?php include("template/cabecera.php"); ?>
+    <div class="jumbotron">
+                <h1 class="display-3">Bienvenidos a una biblioteca gratuita</h1>
+                <p class="lead">Consulta los libros que tenemos para ti</p>
+                <hr class="my-2">
+                <img width="400" src="img/banner.jpg" class="img-thumbnail rounded"/>
+
+                <p>Más información</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver colección de libros</a>
+                </p>
+            </div>
+     <?php include("template/pie.php"); ?>
---
+++ nosotros.php
@@ -0,0 +1,11 @@
+<?php include("template/cabecera.php"); ?>
+
+<div class="jumbotron">
+    <h1 class="display-3">Nosotros</h1>
+    <p class="lead">Hola que tal ya empezamos </p>
+    <hr class="my-2">
+</div>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1325.856071824575!2d-97.21616583960797!3d19.962831504477528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85dae546ec3c806b%3A0xd6365bebe0cbfb9b!2sTlapacoyan!5e0!3m2!1ses-419!2smx!4v1650665510425!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+<?php include("template/pie.php"); ?>
